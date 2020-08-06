1e47418528d77ae4cc157f9a226c5b07ce0dedb5
dreadlockie <dreadlock8907@gmail.com>
2020-08-06 08:40:22 +0300

    split complex object to a number of simple ones

--- medium8-1-4.cs
+++ medium8-1-4.cs
@@ -1,25 +1,49 @@
+using System;
+
 class Player
 {
-    public string Name { get; private set; }
-    public float WeaponCooldown { get; private set; }
-    public float MovementDirectionX { get; private set; }
-    public float MovementDirectionY { get; private set; }
-    public int Age { get; private set; }
-    public int WeaponDamage { get; private set; }
-    public float MovementSpeed { get; private set; }
+  public string Name { get; private set; }
+  public int Age { get; private set; }
+  Mover Mover { get; set; }
+  Weapon Weapon { get; set; }
 
-    public void Move()
-    {
-        //Do move
-    }
+  public void Move()
+  {
+    Mover.DoMove();
+  }
 
-    public void Attack()
-    {
-        //attack
-    }
+  public void Attack()
+  {
+    if(Weapon.IsReloading()) 
+      return;
+    Weapon.Attack();
+  }
+}
 
-    public bool IsReloading()
-    {
-        throw new NotImplementedException();
-    }
+class Mover
+{
+  public float DirectionX { get; private set; }
+  public float DirectionY { get; private set; }
+  public float Speed { get; private set; }
+
+  public void DoMove()
+  {
+    // Do move
+  }
+}
+
+class Weapon
+{
+  public float Cooldown { get; private set; }
+  public int Damage { get; private set; }
+
+  public void Attack()
+  {
+    //attack
+  }
+  
+  public bool IsReloading()
+  {
+    throw new NotImplementedException();
+  }
 }
